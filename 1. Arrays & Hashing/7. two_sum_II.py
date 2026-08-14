45740db21bdc2639bc8dae5744c8020524dbcb04
Sid1813 <sidhu2sharp@gmail.com>
2026-08-14 21:25:27 +0530

    added solutions

--- 1. Arrays & Hashing/7. two_sum_II.py
+++ 1. Arrays & Hashing/7. two_sum_II.py
@@ -1,5 +1,27 @@
 # Find two numbers in the array that add up to the target and return their indices; return indices in 1-based indexing.
 
+# underlying 2 pointer pattern DSA solution :
+
+class Solution:
+
+    def twoSum(self, numbers, target):
+
+        left = 0
+        right = len(numbers) - 1
+
+        while(left < right):
+
+            if(numbers[left] + numbers[right] < target):
+                left += 1
+
+            elif(numbers[left] + numbers[right] > target):
+                right -= 1
+
+            else:
+                return left + 1, right + 1
+            
+# slightly updated unordered 2 sum solution :
+
 class Solution:
 
     def twoSum(self, nums, target):
