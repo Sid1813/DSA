1b9284f83194abb8d7fe2a9cce8dc5b79e7d440d
Sid1813 <sidhu2sharp@gmail.com>
2026-09-06 18:17:14 +0530

    refactor longestCommonPrefix method for clarity and consistency in comments

--- 1. Arrays & Hashing/6. longest_common_prefix.py
+++ 1. Arrays & Hashing/6. longest_common_prefix.py
@@ -21,15 +21,15 @@ class Solution:
         lst = []; common_prefix = ""
         smallest = min(words, key = len)
 
-        for i in range(len(smallest)):
+        for i in range(len(smallest)): # len(smallest) because common prefix CANNOT be longer than the smallest word in words
             for word in words:
-                lst.append(word[i])
+                lst.append(word[i]) # This inner loop iterates through all words and appends letter at index 0,1,2... len(word)-1 of each word in list - words
             if(len(set(lst)) == 1):
-                common_prefix += word[i]
+                common_prefix += word[i] # if the if condition holds true, that clearly means that the letter at i index is same/common for all words, therefore add it to common prefix
             else:
-                return common_prefix
-            lst = []
-        return common_prefix
+                return common_prefix # if the above if condition does NOT hold true, that means there is a mismatch in letters between the words at index i. Therefore we return whatever common prefix we have so far
+            lst = [] # This is to ensure list - lst starts from scratch to now compare the letters at the next index of each word in words
+        return common_prefix # This return is for, if the else statement had not been triggered even once thoughout all iterations of the loop i.e if the smallest word in wordsis the common prefix amongst every word in words
                 
 
 
